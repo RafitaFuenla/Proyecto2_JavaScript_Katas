56b82af64595f3bfb248eac5b6960e374add1d6a
Rafita <afarfuenla@hotmail.com>
2025-07-29 01:17:26 +0200

    Actualizamos

---
+++ ejer07.js
@@ -0,0 +1,11 @@
+/* EJERCICIO 7 */
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log("(Ejercicio 7)", numberOne);
+  } else {
+    console.log("(Ejercicio 7)", numberTwo);
+  }
+}
+
+greaterNumber(7, 9);
+
