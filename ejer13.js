c84a9cf3c983fbe02f2fc971479c4134a6bb8594
Rafita <afarfuenla@hotmail.com>
2025-07-31 02:00:55 +0200

    no avanzamos

---
+++ ejer13.js
@@ -0,0 +1,81 @@
+/* EJERCICIO 13 */
+/*Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos devuelve un true y la posición de dicho elemento y por la contra un false.
+Puedes usar este array para probar tu función: */
+
+const names = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function nameFinder(nameList) {
+  // Completar
+
+  console.log(names.indexOf("Peter"));
+}
+console.log(names.indexOf("Peter"));
+
+/* EJERCICIO 14 */
+
+/* EJERCICIO 15 */
+
+/* EJERCICIO 16 */
+
+/* EJERCICIO 17 */
+
+/* EJERCICIO 18 */
+
+/* EJERCICIO 19 */
+
+/* EJERCICIO 20 */
+
+/* EJERCICIO 21 */
+
+/* EJERCICIO 22 */
+
+/* EJERCICIO 23 */
+
+/* EJERCICIO 24 */
+
+/* EJERCICIO 25 */
+
+/* EJERCICIO 26 */
+
+/* EJERCICIO 27 */
+
+/* EJERCICIO 28 */
+
+/* EJERCICIO 29 */
+
+/* EJERCICIO 30 */
+
+/* EJERCICIO 31 */
+
+/* EJERCICIO 32 */
+
+/* EJERCICIO 33 */
+
+/* EJERCICIO 34 */
+
+/* EJERCICIO 35 */
+
+/* EJERCICIO 36 */
+
+/* EJERCICIO 37 */
+
+/* EJERCICIO 38 */
+
+/* EJERCICIO 39 */
+
+/* EJERCICIO 40 */
+
+/* EJERCICIO 41 */
+
+/* EJERCICIO 42 */
