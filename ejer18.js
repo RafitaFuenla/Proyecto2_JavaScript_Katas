3517f0bf2d320e8eaa9bda036f012387c6613724
Rafita <afarfuenla@hotmail.com>
2025-08-19 22:00:01 +0200

    algo mas

---
+++ ejer18.js
@@ -0,0 +1,63 @@
+/* EJERCICIO 18 */
+/* Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y el id 40.
+
+Imprime en un console log el array resultante.
+
+Puedes usar este array: */
+
+const placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+
+/* EJERCICIO 19 */
+
+/* EJERCICIO 20 */
+
+/* EJERCICIO 21 */
+
+/* EJERCICIO 22 */
+
+/* EJERCICIO 23 */
+
+/* EJERCICIO 24 */
+
+/* EJERCICIO 25 */
+
+/* EJERCICIO 26 */
+
+/* EJERCICIO 27 */
+
+/* EJERCICIO 28 */
+
+/* EJERCICIO 29 */
+
+/* EJERCICIO 30 */
+
+/* EJERCICIO 31 */
+
+/* EJERCICIO 32 */
+
+/* EJERCICIO 33 */
+
+/* EJERCICIO 34 */
+
+/* EJERCICIO 35 */
+
+/* EJERCICIO 36 */
+
+/* EJERCICIO 37 */
+
+/* EJERCICIO 38 */
+
+/* EJERCICIO 39 */
+
+/* EJERCICIO 40 */
+
+/* EJERCICIO 41 */
+
+/* EJERCICIO 42 */
