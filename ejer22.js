1d014205733bb58c86fdf2e2f651a3b2c174f0ed
Rafita <afarfuenla@hotmail.com>
2025-08-21 04:21:48 +0200

    lo dejamos por hoy

---
+++ ejer22.js
@@ -0,0 +1,67 @@
+/* EJERCICIO 22 */
+/*Usa un for para remplazar todas las comidas que no sean veganas con las frutas del array de frutas.
+
+Recuerda no usar frutas duplicadas.
+
+Finalmente, imprime el array resultante.*/
+
+const fruits = ["Strawberry", "Banana", "Orange", "Apple"];
+
+const noVegan = []
+
+const foodSchedule = [
+  { name: "Heura", isVegan: true },
+  { name: "Salmon", isVegan: false },
+  { name: "Tofu", isVegan: true },
+  { name: "Burger", isVegan: false },
+  { name: "Rice", isVegan: true },
+  { name: "Pasta", isVegan: true },
+];
+
+for (let food of foodSchedule){
+    if (food.isVegan === false){
+
+    }
+
+}
+
+
+/* EJERCICIO 23 */
+
+/* EJERCICIO 24 */
+
+/* EJERCICIO 25 */
+
+/* EJERCICIO 26 */
+
+/* EJERCICIO 27 */
+
+/* EJERCICIO 28 */
+
+/* EJERCICIO 29 */
+
+/* EJERCICIO 30 */
+
+/* EJERCICIO 31 */
+
+/* EJERCICIO 32 */
+
+/* EJERCICIO 33 */
+
+/* EJERCICIO 34 */
+
+/* EJERCICIO 35 */
+
+/* EJERCICIO 36 */
+
+/* EJERCICIO 37 */
+
+/* EJERCICIO 38 */
+
+/* EJERCICIO 39 */
+
+/* EJERCICIO 40 */
+
+/* EJERCICIO 41 */
+
+/* EJERCICIO 42 */
