0f19f7cfec7569c0d3594c6f84b426b555f6dd06
Rafita <afarfuenla@hotmail.com>
2025-08-25 00:02:38 +0200

    LO HICE SOLO!!!!!

--- ejer23.js
+++ ejer23.js
@@ -1,7 +1,9 @@
 /* EJERCICIO 23 */
 /*Usa un bucle para crear 3 arrays de películas filtrados por categorías.
 
-Pelicula pequeña -> menos de 100 minutos, película mediana -> más de 100 minutos y menos de 200 y pelicula grande -> más de 200 minutos.
+Pelicula pequeña -> menos de 100 minutos, 
+película mediana -> más de 100 minutos y menos de 200 y 
+pelicula grande -> más de 200 minutos.
 
 Imprime cada array por consola.*/
 
@@ -18,19 +20,34 @@ const movies = [
   { name: "Eternal Sunshine of the Spotless Mind", durationInMinutes: 108 },
 ];
 
-const smallMovie = []
-const normalMovie = []
-const bigMovie = []
+const smallMovie = [];
+const normalMovie = [];
+const bigMovie = [];
 
-for (let i = 0; i < movies.length; i++){
-    if (movie.durationInMinutes > 100){
-        smallMovie.push(movies.name)
-    }
-
-
+for (let i = 0; i < movies.length; i++) {
+  //Creamos una VARIABLE (i) que comienza en la posicion 0. Recorre toda la constante de movies (lenght) y suma una posicionn mas por cada las propiedades
+  if (movies[i].durationInMinutes < 100) {
+    // Si movies (i es para cada posisicon).duracion es inferior a 100...
+    smallMovie.push(movies[i].name);
+    // Hacemos un push a la constante smallMovies con cada variable que estableza la condicion (menor que 100)
+  }
 }
-console.log(smallMovie)
 
+for (let i = 0; i < movies.length; i++) {
+  if (movies[i].durationInMinutes > 100 && movies[i].durationInMinutes < 200) {
+    normalMovie.push(movies[i].name);
+  }
+}
+
+for (let i = 0; i < movies.length; i++) {
+  if (movies[i].durationInMinutes > 200) {
+    bigMovie.push(movies[i].name);
+  }
+}
+
+console.log(smallMovie);
+console.log(normalMovie);
+console.log(bigMovie );
 
 /* EJERCICIO 24 */
 
