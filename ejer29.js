8fecb06c4964c097e7de3e10f9f939b7b350bfbf
Rafita <afarfuenla@hotmail.com>
2025-08-26 00:51:31 +0200

    algo

---
+++ ejer29.js
@@ -0,0 +1,52 @@
+/* EJERCICIO 29 */
+/* Agrupa las películas de Star Wars por década de lanzamiento en un objeto donde las claves son las décadas y los valores son arrays de películas.
+
+Utiliza bucles para lograrlo e imprime el objeto final por consola. */
+
+const starWarsMovies = [
+  { title: 'A New Hope', releaseYear: 1977 },
+  { title: 'The Empire Strikes Back', releaseYear: 1980 },
+  { title: 'Return of the Jedi', releaseYear: 1983 },
+  { title: 'The Phantom Menace', releaseYear: 1999 },
+  { title: 'Attack of the Clones', releaseYear: 2002 },
+  { title: 'Revenge of the Sith', releaseYear: 2005 },
+  { title: 'The Force Awakens', releaseYear: 2015 },
+  { title: 'The Last Jedi', releaseYear: 2017 },
+  { title: 'The Rise of Skywalker', releaseYear: 2019 },
+  { title: 'Rogue One', releaseYear: 2016 },
+  { title: 'Solo', releaseYear: 2018 }
+];
+
+let decadas = 0
+
+for (const starWarsMovie of starWarsMovies){
+
+}
+
+
+
+/* EJERCICIO 30 */
+
+/* EJERCICIO 31 */
+
+/* EJERCICIO 32 */
+
+/* EJERCICIO 33 */
+
+/* EJERCICIO 34 */
+
+/* EJERCICIO 35 */
+
+/* EJERCICIO 36 */
+
+/* EJERCICIO 37 */
+
+/* EJERCICIO 38 */
+
+/* EJERCICIO 39 */
+
+/* EJERCICIO 40 */
+
+/* EJERCICIO 41 */
+
+/* EJERCICIO 42 */
