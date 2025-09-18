197f8ca6345cccffd77ed7ad2b7b49b7b03de789
Rafita <afarfuenla@hotmail.com>
2025-09-18 02:49:18 +0200

    se va complicando la cosa

---
+++ ejer12b.js
@@ -0,0 +1,36 @@
+/* Ejercicio 12
+Valores únicos: Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados.
+
+Pista: puedes generar un nuevo array y devolverlo.
+
+Puedes usar este array para probar tu función: */
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+
+
+function removeDuplicates(list) {
+    const noRep = []
+    for (const element of list){
+        if (!noRep.includes(element)){
+             noRep.push(element)
+        }
+           
+    }
+    return noRep
+}
+
+
+
+console.log(removeDuplicates(duplicates))
