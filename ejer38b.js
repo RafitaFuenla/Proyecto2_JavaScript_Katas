0248f7c0164c31928e23362212fa5a302bcbfaeb
Rafita <afarfuenla@hotmail.com>
2025-10-01 00:13:14 +0200

    no me sale,...IA, IA y mas IA :(

---
+++ ejer38b.js
@@ -0,0 +1,60 @@
+  /* Ejercicio 38
+  Dado el siguiente código usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+
+  Es decir, la media de volumen de todos los volumenes juntos. */
+
+  const users = [
+    {
+      name: "Alberto",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Antonio",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Santiago",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Laura",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ];
+
+const volumenes = [];
+
+
+for (const user of users) {
+  for (const sound in user.favoritesSounds) {
+    volumenes.push(user.favoritesSounds[sound].volume);
+  }
+}
+
+
+let suma = 0;
+for (const volumen of volumenes) {
+  suma += volumen;
+}
+
+
+const media = suma / volumenes.length;
+
+
+console.log(`La media del volumen es: ${media}`)
